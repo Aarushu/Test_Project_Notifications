5799490bf30a54e4593508ca63e23177c185163a
Aarushu <96773012+Aarushu@users.noreply.github.com>
2023-03-26 22:29:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const button = document.querySelector("button")
+
+button. addEventListener("click", () => {
+  Notification.requestPermission().then(perm => {
+  if (perm === "granted") {
+    const notification = new Notification("Example notification", {
+        body: "This is more Text",
+        data: {hello: "World"},
+        icon: "Logo Centered.png",
+        tag: "Welcome Message",
+        vibrate: "",
+    })
+
+    notification.addEventListener("error", e => {
+        alert("error")
+    })
+       if (perm === "granted") {
+
+    }
+  }
+})
+})
+
+let notification
+let interval
+document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === "hidden") {
+       const leaveDate = new Date()
+       interval = setInterval(() => {
+       notification = new Notification("Come back please", {
+            body: `You have been gone for ${Math.round((new Date() - leaveDate) / 1000)} seconds`,
+            tag: "Come back",
+        })
+    }, 100)
+    } else {
+        clearInterval(interval)
+        notification.close()
+    }
+})
